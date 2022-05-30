cf53131b89134cd11e8a7e72e0c75ba564e3ba48
CodeWizrd <codewizard001@gmail.com>
2022-05-31 05:18:14 +0530

    Rendering todo list

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 App = {
+  loading : false,
+  contracts: {},
+
   load: async () => {
     console.log('App.load');
 
     await App.loadWeb3();
     await App.loadAccount();
+    await App.loadContract();
+    await App.render();
   } , 
   loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
@@ -45,6 +50,89 @@ App = {
 
     console.log('Account: ' + App.account);
   },
+
+  loadContract: async () => {
+    // Create a JavaScript version of the smart contract
+    const todoList = await $.getJSON('TodoList.json')
+    App.contracts = {}
+    App.contracts.TodoList = TruffleContract(todoList)
+    App.contracts.TodoList.setProvider(App.web3Provider)
+
+    // Hydrate the smart contract with values from the blockchain
+    App.todoList = await App.contracts.TodoList.deployed()
+
+    console.log('Contract: ',App.todoList);
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean;
+    const loader = $('#loader');
+    const content = $('#content');
+    if (boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  },
+
+  renderTasks: async () => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount()
+    console.log('Task count: ',taskCount);
+    const $taskTemplate = $('.taskTemplate')
+
+    // Render out each task with a new task template
+    for (var i = 0; i < taskCount; i++) {
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskTitle = task[1]
+      const taskDescription = task[2]
+      const taskCompleted = task[3]
+
+      // Create the html for the task
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskDescription)
+      $newTaskTemplate.find('.title').html(taskTitle)
+      $newTaskTemplate.find('.task-id').html(taskId)
+      $newTaskTemplate.find('.completed').html(taskCompleted)
+
+      console.log('Task: ',task);
+      console.log('TaskCompleted',taskCompleted);
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      }
+      else {
+        $('#taskList').append($newTaskTemplate)
+      }
+
+      // Show the task
+      $newTaskTemplate.show()
+    }
+  },
+
+  render: async () => {
+    // Prevent double render
+    if (App.loading) {
+      return
+    }
+
+    // Update app loading state
+    App.setLoading(true)
+
+    // Render Account
+    $('#account').html(App.account)
+
+    // Render Tasks
+    await App.renderTasks()
+
+    // Update loading state
+    App.setLoading(false)
+  }
 }
 
 $(() => {
